bba38b5bad2e4c870db1b7ecc17926a2dc1d16b4
vladiasa123 <corpodeanvladvlad@gmail.com>
2025-01-05 22:12:32 +0200

    build gradle commit before pulling

--- app/src/main/java/com/example/padel/api/RetrofitClient.kt
+++ app/src/main/java/com/example/padel/api/RetrofitClient.kt
@@ -18,7 +18,7 @@ object RetrofitClient {
         .build()
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl("http://10.0.2.2:8080")
+        .baseUrl("http://192.168.1.170:8080")
         .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
